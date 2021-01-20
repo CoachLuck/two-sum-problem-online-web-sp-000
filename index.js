1cf2a3fcd1ff9cf4e50325f0daa1b45b277b41d5
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 20:47:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let array = [2, 3, 4, 3, 6, 7]
 let sum = 6
-function bruteForceTwoSum() {
+function bruteForceTwoSum(arr, um) {
 
 }
 
@@ -12,7 +12,7 @@ function binaryMatch() {
 
 }
 
-function hashTwoSum(sum, hash) {
+function hashTwoSum(sum, arr) {
   let found = []
 }
 
