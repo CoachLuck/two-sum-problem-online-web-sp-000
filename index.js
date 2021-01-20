d8dd31ed8d32de1f4c685b030998ab68f196aaad
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-20 20:49:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let array = [2, 3, 4, 3, 6, 7]
 let sum = 6
+
 function bruteForceTwoSum(arr, sum) {
 
 }
@@ -9,7 +10,8 @@ function binarySearchTwoSum(arr, sum) {
 }
 
 function binaryMatch(arr, num) {
-
+  let find = arr[0];
+  conosle.log(find)
 }
 
 function hashTwoSum(arr, sum) {
